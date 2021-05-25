e4893603744a72dfbba9af0a226e32803f8107f3
Zalina <Zalinasoria@gmail.com>
2021-05-25 19:23:38 -0400

    shered on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+// created new project
+        //enable version control
+        //added .gitignore . idea , target, iml file
+        //made initial commit
+        // more things
 
     }
 }
